297231c9c6ddd54070ce79e0daedc16d13a19934
DmytroSyniuhin <dmytrosyniuhin@gmail.com>
2023-10-31 22:28:35 +0200

    Homework

--- src/Banks.java
+++ src/Banks.java
@@ -2,9 +2,9 @@ public class Banks {
     double[] value = initializeArrayDouble(14);
     String[] nameBanks = new String[]{"ПРИВАТБАНК", "Сенс Банк", "Сітібанк", "ІНГ Банк Україна", "Ощадбанк", "Укрексімбанк", "УКРГАЗБАНК",
             "Райффайзен Банк", "УКРСИББАНК", "ПУМБ", "КРЕДІ АГРІКОЛЬ БАНК", "Південний", "КРЕДОБАНК", "Банк Восток"};
-    public static double[] initializeArrayDouble(int a) {
+    public static double[] initializeArrayDouble(int size) {
 
-        double[] initializeArrayDouble = new double[a];
+        double[] initializeArrayDouble = new double[size];
         for (int i = 0; i < initializeArrayDouble.length; i++) {
             initializeArrayDouble[i] = 35 + Math.random() * 5;
         }
